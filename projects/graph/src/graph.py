460e66bde605ecd67a0e3ea41da35d84c89e1b1c
theryanclausen <theryanclausen@gmail.com>
2019-03-04 16:16:30 -0700

    recursive dft dfs bfs

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -46,6 +46,7 @@ class Graph:
         while q.size() > 0:
             current = q.dequeue()
             if current not in visited:
+                print('bft',current)
                 visited.add(current)
                 for neighbor in self.vertices[current]:
                     q.enqueue(neighbor)
@@ -57,6 +58,64 @@ class Graph:
         while s.size() > 0:
             current = s.pop()
             if current not in visited:
+                print('dft', current)
                 visited.add(current)
                 for neighbor in self.vertices[current]:
                     s.push(neighbor)
+    
+    def rdft(self, starting_vertex_id, visited=set()):
+        if starting_vertex_id in visited:
+            return
+        visited.add(starting_vertex_id)
+        for neighbor in self.vertices[starting_vertex_id]:
+            self.rdft(neighbor, visited)
+
+    def bfs(self, starting_vertex_id, target):
+        q = Queue()
+        visited = set()
+        q.enqueue(starting_vertex_id)
+        while q.size() > 0:
+            current = q.dequeue()
+            if current == target:
+                return True
+            if current not in visited:
+                visited.add(current)
+                for neighbor in self.vertices[current]:
+                    q.enqueue(neighbor)
+        return False
+
+    def dfs(self, starting_vertex_id, target):
+        s = Stack()
+        visited = set()
+        s.push(starting_vertex_id)
+        while s.size() > 0:
+            current = s.pop()
+            if current == target:
+                return True
+            if current not in visited:
+                visited.add(current)
+                for neighbor in self.vertices[current]:
+                    s.push(neighbor)
+        return False
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('2')
+graph.add_vertex('1')
+graph.add_vertex('3')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_vertex('7')
+graph.add_edge('5', '3')
+graph.add_edge('6', '3')
+graph.add_edge('7', '1')
+graph.add_edge('4', '7')
+graph.add_edge('1', '2')
+graph.add_edge('7', '6')
+graph.add_edge('2', '4')
+graph.add_edge('3', '5')
+graph.add_edge('2', '3')
+graph.add_edge('4', '6')
+
+# graph.rdft('1')
+# print(graph.dfs('1','79'))
