6cd28a5243ead0a7574592a5da16e2324ccfafad
theryanclausen <theryanclausen@gmail.com>
2019-03-04 14:36:57 -0700

    follow along

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,62 @@
 """
 Simple graph implementation
 """
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError('That vertex does not exist')
+    def bft(self, starting_vertex_id):
+        q = Queue()
+        visited = set()
+        q.enqueue(starting_vertex_id)
+        while q.size() > 0:
+            current = q.dequeue()
+            if current not in visited:
+                visited.add(current)
+                for neighbor in self.vertices[current]:
+                    q.enqueue(neighbor)
+
+    def dft(self, starting_vertex_id):
+        s = Stack()
+        visited = set()
+        s.push(starting_vertex_id)
+        while s.size() > 0:
+            current = s.pop()
+            if current not in visited:
+                visited.add(current)
+                for neighbor in self.vertices[current]:
+                    s.push(neighbor)
